b1bc477f0c88ffdb756d5d22fb2ff9d8c13f14fb
samurrai <samurrrai.r@gmail.com>
2019-02-19 13:26:03 +0600

    Домашняя работа по XML и JSON

---
+++ Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLAndJsonHomework
+{
+    public class Car
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int MaxSpeed { get; set; }
+        public string Color { get; set; }
+    }
+}
